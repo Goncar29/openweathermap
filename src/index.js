649250787cc562ea6ad004a0a1a5096f91221b3d
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-10-04 16:10:32 -0300

    Se realiza un fecth para traer datos de temp, nombre ciudad y el icono, mostrandolos en el DOM

--- src/index.js
+++ src/index.js
@@ -3,4 +3,31 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+// console.log('Happy hacking :)')
+const idCity = '3441572'
+const key = '24a2daea82cd2f605739444659d81ef8'
+const url = `https://api.openweathermap.org/data/2.5/weather?id=${idCity}&appid=${key}`
+
+window
+    .fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((responseJson) => {
+        const todosLosItems = []
+
+        console.log(responseJson.main.temp);
+
+        const ciudad = document.createElement('h2');
+        ciudad.textContent = responseJson.name;
+        const imagen = document.createElement('img');
+        imagen.src = `http://openweathermap.org/img/wn/${responseJson.weather[0].icon}@2x.png`;
+        const temperatura = document.createElement('h4')
+        temperatura.textContent = responseJson.main.temp;
+
+        const contenedor = document.createElement('div')
+
+        contenedor.append(ciudad, imagen, temperatura)
+
+        todosLosItems.push(contenedor)
+
+        document.body.append(...todosLosItems)
+    })
