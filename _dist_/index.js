8c59c824fc7a3570a9d85f844a5bd8dce8c65dd2
Goncar29 <Goncar29@users.noreply.github.com>
2022-10-05 20:09:19 +0000

    deploy: 151350f349e342d38222f1c12e40f4786233315f

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,13 +1,8 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
-// console.log('Happy hacking :)')
 const idCity = '3441572'
 const key = '24a2daea82cd2f605739444659d81ef8'
 const url = `https://api.openweathermap.org/data/2.5/weather?id=${idCity}&appid=${key}`
 
+const kelvin = 273.15
 window
     .fetch(url)
     .then((respuesta) => respuesta.json())
@@ -19,15 +14,21 @@ window
         const ciudad = document.createElement('h2');
         ciudad.textContent = responseJson.name;
         const imagen = document.createElement('img');
-        imagen.src = `http://openweathermap.org/img/wn/${responseJson.weather[0].icon}@2x.png`;
-        const temperatura = document.createElement('h4')
-        temperatura.textContent = responseJson.main.temp;
+        imagen.src = `http://openweathermap.org/img/wn/${responseJson.weather[0].icon}@4x.png`;
+        const temperatura = document.createElement('h5')
+        const minTemp = document.createElement('h5')
+        const maxTemp = document.createElement('h5')
+        temperatura.textContent = `Temp ${(responseJson.main.temp - kelvin).toFixed(1)}°C`;
+        minTemp.textContent = ` Min ${(responseJson.main.temp_min)}°C`;
+        maxTemp.textContent = ` Max ${(responseJson.main.temp_max - kelvin).toFixed(1)}°C`;
 
+        const temp = document.createElement('span')
         const contenedor = document.createElement('div')
 
-        contenedor.append(ciudad, imagen, temperatura)
+        temp.append(temperatura, minTemp, maxTemp)
+        contenedor.append(ciudad, imagen, temp)
 
         todosLosItems.push(contenedor)
 
-        document.body.append(...todosLosItems)
+        document.querySelector("body > main").append(...todosLosItems)
     })
