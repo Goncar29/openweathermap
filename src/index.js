deb192ecd7db7f9dde736125e79905ddf123a079
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-10-13 15:10:41 -0300

    se comienza a trabajar en el buscador de ciudad

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const key = '24a2daea82cd2f605739444659d81ef8'
 const url = `https://api.openweathermap.org/data/2.5/weather?id=${idCity}&appid=${key}`
 
 const kelvin = 273.15
+
 window
     .fetch(url)
     .then((respuesta) => respuesta.json())
