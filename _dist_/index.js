fb28faca9f1cff1939ba0e5a2f3c428b456f5d8a
Goncar29 <Goncar29@users.noreply.github.com>
2022-10-13 18:11:26 +0000

    deploy: deb192ecd7db7f9dde736125e79905ddf123a079

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,6 +3,7 @@ const key = '24a2daea82cd2f605739444659d81ef8'
 const url = `https://api.openweathermap.org/data/2.5/weather?id=${idCity}&appid=${key}`
 
 const kelvin = 273.15
+
 window
     .fetch(url)
     .then((respuesta) => respuesta.json())
@@ -15,18 +16,28 @@ window
         ciudad.textContent = responseJson.name;
         const imagen = document.createElement('img');
         imagen.src = `http://openweathermap.org/img/wn/${responseJson.weather[0].icon}@4x.png`;
+        
+        const description = document.createElement('h5')
+        description.textContent = `${responseJson.weather[0].description}`
+        console.log(responseJson.weather[0].description)
+
+        const humedad = document.createElement('h5')
+        humedad.textContent = `Humedad ${responseJson.main.humidity}%`
+        console.log(responseJson.main.humidity)
+
         const temperatura = document.createElement('h5')
         const minTemp = document.createElement('h5')
         const maxTemp = document.createElement('h5')
         temperatura.textContent = `Temp ${(responseJson.main.temp - kelvin).toFixed(1)}°C`;
-        minTemp.textContent = ` Min ${(responseJson.main.temp_min)}°C`;
+        minTemp.textContent = ` Min ${(responseJson.main.temp_min - kelvin).toFixed(1)}°C`;
         maxTemp.textContent = ` Max ${(responseJson.main.temp_max - kelvin).toFixed(1)}°C`;
 
         const temp = document.createElement('span')
         const contenedor = document.createElement('div')
+        contenedor.classList.add('cards')
 
         temp.append(temperatura, minTemp, maxTemp)
-        contenedor.append(ciudad, imagen, temp)
+        contenedor.append(ciudad, imagen, description, humedad, temp)
 
         todosLosItems.push(contenedor)
 
