2873c5c4c9899643035cd4c1a2d7f4eed3251ede
Goncar29 <Goncar29@users.noreply.github.com>
2022-10-14 22:51:34 +0000

    deploy: 2aed4c310fcbf290be38fb10ac1adeb303bc2077

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,16 +1,19 @@
-const idCity = '3441572'
+const obtenerClima = document.querySelector('#obtenerClima')
+const ciudad = document.querySelector('#ciudad')
 const key = '24a2daea82cd2f605739444659d81ef8'
-const url = `https://api.openweathermap.org/data/2.5/weather?id=${idCity}&appid=${key}`
+const unidad = 'metric'
 
-const kelvin = 273.15
+obtenerClima.addEventListener('click', e => {
+    e.preventDefault();
 
-window
+    const ciudadValue = ciudad.value
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudadValue}&lang=es&appid=${key}&units=${unidad}`
+    
+    window
     .fetch(url)
     .then((respuesta) => respuesta.json())
     .then((responseJson) => {
         const todosLosItems = []
-
-        console.log(responseJson.main.temp);
 
         const ciudad = document.createElement('h2');
         ciudad.textContent = responseJson.name;
@@ -28,9 +31,9 @@ window
         const temperatura = document.createElement('h5')
         const minTemp = document.createElement('h5')
         const maxTemp = document.createElement('h5')
-        temperatura.textContent = `Temp ${(responseJson.main.temp - kelvin).toFixed(1)}°C`;
-        minTemp.textContent = ` Min ${(responseJson.main.temp_min - kelvin).toFixed(1)}°C`;
-        maxTemp.textContent = ` Max ${(responseJson.main.temp_max - kelvin).toFixed(1)}°C`;
+        temperatura.textContent = `Temp ${(responseJson.main.temp)}°C`;
+        minTemp.textContent = ` Min ${(responseJson.main.temp_min)}°C`;
+        maxTemp.textContent = ` Max ${(responseJson.main.temp_max)}°C`;
 
         const temp = document.createElement('span')
         const contenedor = document.createElement('div')
@@ -41,5 +44,7 @@ window
 
         todosLosItems.push(contenedor)
 
-        document.querySelector("body > main").append(...todosLosItems)
+        document.querySelector("body > main > div").append(...todosLosItems)
     })
+})
+
