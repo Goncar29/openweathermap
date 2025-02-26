282ce59bb845c05c5093c29fbe201bbbf5763a88
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2025-02-26 15:18:15 -0300

    Terminado con el deploy en gh-pages, se configura enlaces css y js para buen funcionamiento

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   mount: {
-    public: { url: '/', static: true },  // Copia archivos tal cual a la raíz de `build/`
-    src: { url: '/dist' }  // Transforma los archivos JS de `src/` y los coloca en `/dist`
+    public: { url: '/', static: true },
+    src: { url: '/dist' }
   },
   buildOptions: {
-    out: 'build', // Asegura que Snowpack genere la salida en `build/`
-    baseUrl: './', // Usa rutas relativas para evitar errores en GitHub Pages
-    clean: true // Elimina archivos viejos en `build/` antes de generar nuevos
+    out: 'build',
+    baseUrl: './',
+    clean: true
   }
 };
